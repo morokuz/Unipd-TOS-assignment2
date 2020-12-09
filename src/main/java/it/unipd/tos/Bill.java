7cac7872cfd7c6dafdfad0c3ed4f3d126513153b
moroku <moroku96@gmail.com>
2020-12-09 10:42:23 +0100

    close #3

--- src/main/java/it/unipd/tos/Bill.java
+++ src/main/java/it/unipd/tos/Bill.java
@@ -8,4 +8,12 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Bill implements TakeAwayBill {}
+public class Bill implements TakeAwayBill {
+  public static double getTotal(List<MenuItem> itemsOrdered) {
+    double total = 0;
+    for (int i = 0; i < itemsOrdered.size(); i++) {
+      total += itemsOrdered.get(i).getPrice();
+    }
+    return total;
+  }
+}
